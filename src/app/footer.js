d5064cccf234feb5a7feaf5ebd453706358979a9
VOSANDI2003 <vosandiwickramarathne@gmail.com>
2024-11-12 14:48:54 +0530

    Update footer.js

--- src/app/footer.js
+++ src/app/footer.js
@@ -11,37 +11,37 @@ export default function Footer() {
                 <div>
                     <h4>Use cases</h4>
                     <ul>
-                        <li><Link href="#">UI design</Link></li>
-                        <li><Link href="#">UX design</Link></li>
-                        <li><Link href="#">Wireframing</Link></li>
-                        <li><Link href="#">Diagramming</Link></li>
-                        <li><Link href="#">Brainstorming</Link></li>
-                        <li><Link href="#">Online whiteboard</Link></li>
-                        <li><Link href="#">Team collaboration</Link></li>
+                        <li><Link href="#">Canvas</Link></li>
+                        <li><Link href="#"> Blackboard</Link></li>
+                        <li><Link href="#"> Moodle</Link></li>
+                        <li><Link href="#">Edmodo</Link></li>
+                        <li><Link href="#">Google</Link></li>
+                        <li><Link href="#"> Microsoft Teams for Education</Link></li>
+                        <li><Link href="#"> Socrative</Link></li>
                     </ul>
                 </div>
                 <div>
                     <h4>Explore</h4>
                     <ul>
-                        <li><Link href="#">Design</Link></li>
-                        <li><Link href="#">Prototyping</Link></li>
-                        <li><Link href="#">Development features</Link></li>
-                        <li><Link href="#">Design systems</Link></li>
-                        <li><Link href="#">Collaboration features</Link></li>
-                        <li><Link href="#">Design process</Link></li>
-                        <li><Link href="#">FigJam</Link></li>
+                        <li><Link href="#">Moodle</Link></li>
+                        <li><Link href="#">Google Classroom</Link></li>
+                        <li><Link href="#">Canvas by Instructure</Link></li>
+                        <li><Link href="#">Blackboard Learn</Link></li>
+                        <li><Link href="#">Schoology</Link></li>
+                        <li><Link href="#">Edmodo</Link></li>
+                        <li><Link href="#">Microsoft Teams for Education</Link></li>
                     </ul>
                 </div>
                 <div>
                     <h4>Resources</h4>
                     <ul>
-                        <li><Link href="#">Blog</Link></li>
-                        <li><Link href="#">Best practices</Link></li>
-                        <li><Link href="#">Colors</Link></li>
-                        <li><Link href="#">Color wheel</Link></li>
-                        <li><Link href="#">Support</Link></li>
-                        <li><Link href="#">Developers</Link></li>
-                        <li><Link href="#">Resource library</Link></li>
+                        <li><Link href="#">EdTick</Link></li>
+                        <li><Link href="#">Canvas by Instructure</Link></li>
+                        <li><Link href="#">SC Training</Link></li>
+                        <li><Link href="#">LearnWorlds</Link></li>
+                        <li><Link href="#">Edvance360</Link></li>
+                        <li><Link href="#">Sakai</Link></li>
+                        <li><Link href="#">TalentLMS</Link></li>
                     </ul>
                 </div>
             </div>
